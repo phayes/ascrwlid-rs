4fe81ad538cf6227f4fef9a3ad4fd15befe6d7f4
phayes <patrick.d.hayes@gmail.com>
2020-10-01 19:41:24 -0700

    distances function, in parralel

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
+extern crate ndarray as nd;
+
+use nd::Array2;
 use rand::{thread_rng, Rng};
 use rayon::prelude::*;
+
+// Regular Matrix
+type Matrix = Array2<f64>;
 
 // Find the index of any value that matches. It will not nessisarily find the fist value.
 fn which_c(dat: &[i32], find: i32) -> Option<usize> {
@@ -30,6 +36,31 @@ pub fn sample_int<R: Rng>(weights: &[f64], n: usize, mut rng: R) -> usize {
         accumulated_weights += weights[i];
     }
     i
+}
+
+// Calculate all Euclidean distances between two matrices.
+//  @param a Matrix of Mask centroids.
+//  @param b Matrix of Traps.
+//  @return Returns matrix with distance from each trap to each mask centroid.
+pub fn distances(a: &Matrix, b: &Matrix) -> Matrix {
+    let n_a = a.nrows();
+    let n_b = b.nrows();
+    let total_iter = n_a * n_b;
+
+    // Do the calcuations in parallel on multiple cores
+    let mut out_vec = vec![0.0; total_iter];
+    out_vec
+        .par_iter_mut()
+        .enumerate()
+        .map(|(vec_i, val)| {
+            let i = vec_i / n_a;
+            let j = vec_i % n_a;
+            let base = (a[[i, 0]] - b[[j, 0]]).powi(2) + (a[[i, 1]] - b[[j, 1]]).powi(2);
+            *val = base.powf(0.5)
+        })
+        .collect::<Vec<_>>();
+
+    Matrix::from_shape_vec((n_a, n_b), out_vec).expect("Invalid results for desired shape")
 }
 
 #[cfg(test)]
